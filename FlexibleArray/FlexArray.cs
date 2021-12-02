87895e8c0ebea2dcc53414d82964110b4ad7de8d
MichalStehlik <michal.stehlik@pslib.cz>
2021-12-02 13:30:49 +0100

    FlexArray01

---
+++ FlexibleArray/FlexArray.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexibleArray
+{
+    public class FlexArray : IFlexArray, IEnumerable
+    {
+        protected object[] _array = new object[0];
+        public int Count { get 
+            {
+                int cnt = 0;
+                for (int i = 0; i < Length; i++)
+                {
+                    if (_array[i] != null) cnt++;
+                }
+                return cnt;
+            } }
+
+        public int Length { get { return _array.Length; } }
+
+        public void Add(object o)
+        {
+            Resize(ref _array, Length+1);
+            _array[Length-1] = o;
+        }
+
+        public object Get(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(int index, object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void Resize(ref object[] old, int newSize)
+        {
+            if (newSize < 0) throw new ArgumentOutOfRangeException("Pole nemůže mít zápornou velikost");
+            object[] newArray = new object[newSize];
+            for (int i = 0; i < Math.Min(old.Length,newSize); i++)
+            {
+                newArray[i] = old[i];
+            }
+            old = newArray;
+        }
+    }
+}
---
+++ FlexibleArray/IFlexArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexibleArray
+{
+    public interface IFlexArray
+    {
+        public void Add(object o);
+        public object Get(int index);
+        public void Set(int index, object o);
+        public int IndexOf(object o);
+        public object[] GetAll();
+        public int Count { get; }
+        public int Length { get; }
+    }
+}
