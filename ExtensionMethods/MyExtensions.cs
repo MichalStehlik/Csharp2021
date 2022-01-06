d8225a89fa7af96b314bca38085c94e88ea6f144
Michal Stehlík <michal.stehlik@pslib.cz>
2022-01-06 13:27:23 +0100

    extensions end

--- ExtensionMethods/MyExtensions.cs
+++ ExtensionMethods/MyExtensions.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace ExtensionMethods
 {
-    internal class MyExtensions
+    public static class MyExtensions
     {
+        public static bool IsCountEven<T>(this List<T> items)
+        {
+            return (items.Count() % 2 == 0);
+        }
+
+        public static List<string> ShorterThen(this List<string> items, int length)
+        {
+            var n = new List<string>();
+            foreach (var x in items)
+            {
+                if (x.Length < length) n.Add(x);
+            }
+            return n;
+        }
+
+        public static List<T> Even<T>(this List<T> items)
+        {
+            var n = new List<T>();
+            for (int i = 0; i < items.Count(); i++)
+            {
+                if (i % 2 == 0) n.Add(items[i]);
+            }
+            return n;
+        }
+
+        public static List<T> Condition<T>(this List<T> items, Func<T, bool> cond)
+        {
+            var n = new List<T>();
+            foreach (var x in items)
+            {
+                if (cond(x)) n.Add(x);
+            }
+            return n;
+        }
     }
 }
--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -5,6 +5,8 @@
 //
 // Pokusme se rozšířit obyčejný List (IEnumerable) o nové metody, které původně určitě neobsahoval
 
+using ExtensionMethods;
+
 List<string> strings = new List<string> // určitě neobsahuje metody IsCountEven(), Even(), ShorterThen(int)
 {
     "Anna",
@@ -23,31 +25,34 @@ foreach (string s in strings)
 
 // První metoda bude vracet informaci o tom, zda List obsahuje sudý nebo lichý počet položek
 Console.WriteLine("---");
-//Console.WriteLine(strings.IsCountEven());
+// Console.WriteLine(MyExtensions.IsCountEven(strings));
+Console.WriteLine(strings.IsCountEven());
 
 // Druhá metody bude vracet jen ty položky Listu, které jsou kratší než daný počet znaků
 Console.WriteLine("---");
 
-/*foreach (string s in strings.ShorterThen(5))
+foreach (string s in strings.ShorterThen(5))
 {
     Console.WriteLine(s);
 }
-*/
 
 // Třetí metoda bude vracet jen sudé položky Listu
 Console.WriteLine("---");
-/*
+
 foreach (string s in strings.Even())
 {
     Console.WriteLine(s);
 }
-*/
+
 
 // Čtvrtá metoda bude vracet jen ty položky Listu, které splní podmínku danou Lambda výrazem
 Console.WriteLine("---");
-/*
 foreach (string s in strings.Condition(listItem => listItem.Length > 6))
 {
     Console.WriteLine(s);
 }
-*/
+
+foreach (string s in strings.Condition(listItem => listItem.Length < 5))
+{
+    Console.WriteLine(s);
+}
